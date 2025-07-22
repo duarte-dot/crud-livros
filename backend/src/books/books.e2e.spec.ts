279dbcc17bd0f2294f84c2c4bedbdfe9fd20a34d
duarte <gabrieldvr@outlook.com>
2025-07-22 12:38:35 -0300

    fix: mocking database so it wont try to connect to it inside the tests

--- backend/src/books/books.e2e.spec.ts
+++ backend/src/books/books.e2e.spec.ts
@@ -2,34 +2,44 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { BooksModule } from './books.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('BooksModule (e2e)', () => {
   let app: INestApplication;
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let prismaServiceMock: PrismaService;
+
   beforeAll(async () => {
+    // Criando o mock do PrismaService
+    const prismaServiceMockValue = {
+      book: {
+        create: jest.fn().mockResolvedValue({
+          id: '123',
+          title: 'Book Test',
+          author: 'Author Test',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }),
+        findMany: jest.fn().mockResolvedValue([]),
+      },
+    };
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [BooksModule],
     })
-      .overrideProvider('PrismaService')
-      .useValue({
-        book: {
-          create: jest.fn().mockResolvedValue({
-            id: '123',
-            title: 'Book Test',
-            author: 'Author Test',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          }),
-          findMany: jest.fn().mockResolvedValue([]),
-        },
-      })
+      .overrideProvider(PrismaService)
+      .useValue(prismaServiceMockValue)
       .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    prismaServiceMock = moduleFixture.get<PrismaService>(PrismaService);
   });
 
-  it('/books (GET)', () => {
+  it('/books (GET)', async () => {
+    // Teste para garantir que a resposta seja um array vazio
     return request(app.getHttpServer()).get('/books').expect(200).expect([]);
   });
 
